d8f3c221cdbc78937f966d89176fead900dc6ede
gebretewodros73 <gebretewodros73@gmail.com>
2023-03-15 19:40:53 -0700

    search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sque_mat = matrix
-    new_matrix = sque_mat
+    new_matrix = matrix.copy()
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
-            sque_mat[i][j] = new_matrix[i][j] ** 2
-    return sque_mat
+            new_matrix[i][j] = matrix[i][j] ** 2
+    return new_matrix
