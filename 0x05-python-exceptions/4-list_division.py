d64003da8d12c2469991713fbe56f24691cc8d55
gebretewodros73 <gebretewodros73@gmail.com>
2023-03-28 05:18:11 +0300

    divide element by elemet 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,35 +1,17 @@
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
+    result = []
     for i in range(list_length):
         try:
-            quotient = 0
-            if i < len(my_list_1):
-                element_1 = my_list_1[i]
-                if type(element_1) not in [int, float]:
-                    print("wrong type")
-                    continue
-            else:
-                print("out of range")
-                continue
-                
-            if i < len(my_list_2):
-                element_2 = my_list_2[i]
-                if type(element_2) not in [int, float]:
-                    print("wrong type")
-                    continue
-                elif element_2 == 0:
-                    print("division by 0")
-                    continue
-                else:
-                    quotient = element_1 / element_2
-            else:
-                print("out of range")
-                continue
-                
-            new_list.append(quotient)
-            
+            div = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
-    
-    return new_list
+            div = 0
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            result.append(div)
+    return result
