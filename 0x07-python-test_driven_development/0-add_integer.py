4896032f278f5ef821217570a55e4e569bebdff4
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-06 19:28:52 +0300

    function to add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,12 @@
 #!/bin/usr/python3
+""" define add_integer """
+
+
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-
-    Args:
-    a: integer
-    b: integer (default 98)
-
-    Returns:
-    The sum of a and b
+    """Adds two integers.
 
     Raises:
-    TypeError: if either a or b is not an integer
+        TypeError: if either a or b is not an integer
 
     """
     if not isinstance(a, (int, float)):
