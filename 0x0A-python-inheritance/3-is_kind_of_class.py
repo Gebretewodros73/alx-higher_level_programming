c819f933aa7f10f7fa85b3d940ec5cc07b44bcfd
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-11 21:37:42 +0300

    rearrange the return and amend the documentation

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,8 +6,14 @@ Module documentation - identify if is in instance or in class
 
 def is_kind_of_class(obj, a_class):
     """
-    returns True if it is an instance or class of the given
+    Determines if an obect is an instance or a subclass of a given class
+
+    Args:
+        obj (any): The object to be checked.
+        a_class (class): The class to check against.
+
+    Returns:
+        bool: True if the object is an instance or subclass of the class
     """
 
-    if isinstance(obj, a_class):
-        return True
+    return isinstance(obj, a_class)
