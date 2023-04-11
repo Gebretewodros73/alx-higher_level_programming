e3ba885f9ed9b041b70e5e5c15d7a67a4ba7c1e5
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-11 05:24:20 +0300

    list of available attributes of objects

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def lookup(objec):
+    """
+    Return the list of available attributes and methods of and objects.
+
+
+    Args:
+        objec: The object to lookup.
+
+
+    Return:
+        A list of strings containing the available attributes and methods
+    """
     return [att for att in dir(objec)]
