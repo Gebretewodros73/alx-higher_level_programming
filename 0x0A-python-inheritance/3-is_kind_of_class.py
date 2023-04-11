c7382dcf045983fb109dbf1678f21af5847a4882
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-11 21:29:59 +0300

    documentation

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,7 @@ Module documentation - identify if is in instance or in class
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if it is an instance or class of the given
+    returns True if it is an instance or class of the given
     """
 
     if isinstance(obj, a_class):
