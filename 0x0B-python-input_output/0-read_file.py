73c660d1d5d2988650e469b86b3c6bfd6c091836
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-12 05:53:55 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     reads a text file (UTF8) and prints it to stdout
     """
 
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+    with open(filename, mode='r', encoding='utf-8') as file:
+        print(file.read(), end="")
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-import json
-
 """
-Module documentation - returns the JSON representation
+Module documentation - a function to return the JSON representation of and object(string)
 """
 
 
 def to_json_string(my_obj):
     """
-    in python object representation of json
+    return the JSON representation
     """
-    return json.dumps(my_obj)
+    if isinstance(my_obj, (str, int, float, bool)) or my_obj is None:
+        return json.dumps(my_obj)
+    elif isinstance(my_obj, (list, tuple)):
+        return '[' + ', '.join(to_json_string(elem) for elem in my_obj) + ']'
+    elif isinstance(my_obj, dict):
+        return '{' + ', '.join(f'"{key}": {to_json_string(value)}' for key, value in my_obj.item()) + '}'
+    else:
+        raise TypeError(f"Object of type '{type(my_obj).__name__}' is not JSON serializable")
