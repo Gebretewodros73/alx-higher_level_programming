a627e83495a2c11edbc1e4c9297ecacbd284e07e
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-12 17:57:28 +0300

    a function that return JSon representation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import json
-
 """
 Module documentation - returns the JSON representation
 """
@@ -10,4 +8,17 @@ def to_json_string(my_obj):
     """
     in python object representation of json
     """
-    return json.dumps(my_obj)
+    if my_obj is None:
+        return ""
+    elif isinstance(my_obj, (int, float)):
+        return str(my_obj)
+    elif isinstance(my_obj, str):
+        return '"' + my_obj + '"'
+    elif isinstance(my_obj, (list, tuple)):
+        json_list = [to_json_string(elem) for elem in my_obj]
+        return "[" + ", ".join(json_list) + "]"
+    elif isinstance(my_obj, dict):
+        json_dict = ['"{}": {}'.format(k, to_json_string(v)) for k, v in my_obj.items()]
+        return "{" + ", ".join(json_dict) + "}"
+    elif isinstance(my_obj, bool):
+        return "true" if my_obj else "false"
