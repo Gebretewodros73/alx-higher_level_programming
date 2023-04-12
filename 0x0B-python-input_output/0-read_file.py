275a6563ad8a62d2df77acc8417953ba837b5e7c
tewodros Gebre <117747135+Gebretewodros73@users.noreply.github.com>
2023-04-12 10:43:09 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,6 @@ def read_file(filename=""):
     reads a text file (UTF8) and prints it to stdout
     """
 
-    with open(filename, mode='r', encoding='utf-8') as file:
-        print(file.read(), end="")
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+           print(line, end='')
