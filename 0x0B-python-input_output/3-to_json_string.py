2c5a32842e91c4d791b84f2c0a2800718c9e7f4c
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-12 05:45:52 +0300

    representation of json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+Module documentation - returns the JSON representation
+"""
+
+
+def to_json_string(my_obj):
+    """
+    in python object representation of json
+    """
+    return json.dumps(my_obj)
