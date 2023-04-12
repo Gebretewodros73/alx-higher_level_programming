5bc98edb77bcd30b37ba5a6fa9c0dcea788a5206
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-12 17:09:29 +0300

    import JSON and implement the representation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
+import json
+
 """
-Module documentation - a function to return the JSON representation of and object(string)
+Module documentation - returns the JSON representation
 """
 
 
 def to_json_string(my_obj):
     """
-    return the JSON representation
+    in python object representation of json
     """
-    if isinstance(my_obj, (str, int, float, bool)) or my_obj is None:
-        return json.dumps(my_obj)
-    elif isinstance(my_obj, (list, tuple)):
-        return '[' + ', '.join(to_json_string(elem) for elem in my_obj) + ']'
-    elif isinstance(my_obj, dict):
-        return '{' + ', '.join(f'"{key}": {to_json_string(value)}' for key, value in my_obj.item()) + '}'
-    else:
-        raise TypeError(f"Object of type '{type(my_obj).__name__}' is not JSON serializable")
+    return json.dumps(my_obj)
