054dbc9ae04b663c7b293bd5766e91397c672ebf
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-17 17:29:12 +0300

    add jaso.dumps to base class"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 Module documentation : base class to all other
 classes which handle id attribute
 """
+import json
 
 
 class Base:
@@ -23,3 +24,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns [] if empty else JSON string representation
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
