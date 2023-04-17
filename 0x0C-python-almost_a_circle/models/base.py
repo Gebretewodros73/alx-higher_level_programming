4a86a2a1ef0a07035d5fade24282ea4dc16c6190
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-17 17:47:31 +0300

    implement json.loads to the base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,10 @@ class Base:
         list_dicts = [obj.to_dictionary() for obj in list_objs]
         with open(filename, "w") as file:
             file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ return obj to JSON string """
+        if not json_string:
+            return []
+        return json.loads(json_string)
