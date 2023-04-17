d93fe532c772dd8ba9694ce2456c42da55f0abde
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-17 16:18:48 +0300

    define a square module that inherites rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module Documantation - which define square inherited from rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ define square inheriting rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ square instance initialization """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Square representation """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """ get size with getter method """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ set the size with setter method """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """ update key/value representation for square """
+        if args is not None and len(args) != 0:
+            attributes = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attributes[i], arg[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
