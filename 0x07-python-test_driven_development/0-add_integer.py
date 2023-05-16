caabd935ed3649813a82ee237472f72461a5ac6f
gebretewodros73 <gebretewodros73@gmail.com>
2023-05-16 09:27:05 +0300

    commit according to Q&A

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,13 @@
 #!/bin/usr/python3
-""" define add_integer """
-
-
+"""Function that adds 2 integers.
+    Args:
+        a: int
+        b: int, optional, default value is 98
+"""
 def add_integer(a, b=98):
-    """Adds two integers.
-    Raises: TypeError: if either a or b is not an integer
-    """
+    """The addition of a and b as an integer.
+    Raises:
+        TypeError: if a or b is not an integer."""
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
