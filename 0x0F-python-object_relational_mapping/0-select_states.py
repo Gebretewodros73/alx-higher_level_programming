f4992405ef4815d2a45c7884230aab9670c453e3
gebretewodros73 <gebretewodros73@gmail.com>
2023-06-19 04:18:06 +0300

    Lists all states from the database hbtn_0e_0_usa using MySQLdb.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import MySQLdb
+import sys
+
+# Get MySQL connection parameters from command-line arguments
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+# Connect to MySQL server
+db = MySQLdb.connect(user=username, passwd=password, db=database, host='localhost', port=3306)
+
+# Create a cursor object to interact with the database
+cursor = db.cursor()
+
+# Execute the SQL query to retrieve the states
+query = "SELECT * FROM states ORDER BY id ASC"
+cursor.execute(query)
+
+# Fetch all the rows returned by the query
+rows = cursor.fetchall()
+
+# Display the results
+for row in rows:
+    print(row)
+
+# Close the cursor and database connection
+cursor.close()
+db.close()
