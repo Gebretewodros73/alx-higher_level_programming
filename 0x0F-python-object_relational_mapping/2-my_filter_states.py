75ba24fc59b440d8f7eafbaf136f3c26f9418843
gebretewodros73 <gebretewodros73@gmail.com>
2023-06-19 18:41:47 +0300

    Displays states from the database hbtn_0e_0_usa that match a given input using MySQLdb

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Displays states from the database hbtn_0e_0_usa
+that match a given input using MySQLdb"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Get MySQL connection parameters from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(user=username, passwd=password,
+                         db=database, host='localhost', port=3306)
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve the states
+    query = "SELECT * FROM states WHERE name \
+    LIKE BINARY '{}'"
+    cursor.execute(query.format(sys.argv[4]))
+
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
