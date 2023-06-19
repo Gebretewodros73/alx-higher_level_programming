c01e5b2e3057042df5c77fd787508bf7af72f249
gebretewodros73 <gebretewodros73@gmail.com>
2023-06-19 16:26:15 +0300

    Lists all states from the database hbtn_0e_0_usa using MySQLdb.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
+"""
+Script to list all states from a MySQL database using MySQLdb.
+"""
 import MySQLdb
 import sys
 
-# Get MySQL connection parameters from command-line arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
 
-# Connect to MySQL server
-db = MySQLdb.connect(user=username, passwd=password,
-                     db=database, host='localhost', port=3306)
+if __name__ == "__main__":
+    # Get MySQL connection parameters from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-# Create a cursor object to interact with the database
-cursor = db.cursor()
+    # Connect to MySQL server
+    db = MySQLdb.connect(user=username, passwd=password,
+                         db=database, host='localhost', port=3306)
 
-# Execute the SQL query to retrieve the states
-query = "SELECT * FROM states ORDER BY id ASC"
-cursor.execute(query)
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
 
-# Fetch all the rows returned by the query
-rows = cursor.fetchall()
+    # Execute the SQL query to retrieve the states
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
 
-# Display the results
-for row in rows:
-    print(row)
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
 
-# Close the cursor and database connection
-cursor.close()
-db.close()
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
