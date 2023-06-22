53740b820f66bfa600b77d8405e8ef8d076b67d0
gebretewodros73 <gebretewodros73@gmail.com>
2023-06-22 08:29:49 +0300

    creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa.

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Creates the State "California" with the City "San Francisco"
+in the database hbtn_0e_100_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+from relationship_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+            {password}@localhost:3306/{database}')
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    california = State(name="California")
+    san_francisco = City(name="San Francisco")
+    california.cities.append(san_francisco)
+
+    session.add(california)
+    session.add(san_francisco)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a State.
+"""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class State(Base):
+    """
+    Class definition of a State.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states", cascade="all, delete")
