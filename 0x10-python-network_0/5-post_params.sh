b7c6a813698e1dfa6a00d4ccecb5911ddb8a238f
gebretewodros73 <gebretewodros73@gmail.com>
2023-06-30 21:18:39 +0300

    Sends a POST request with parameters to a specified URL and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sends a POST request with parameters to a specified URL and displays the body of the response
 
-curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com.subject=I will always be here for PLD" "$1"
