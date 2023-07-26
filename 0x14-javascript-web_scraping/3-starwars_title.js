b2f758cfcad80f8e47bb777964589f7a2011e420
gebretewodros73 <gebretewodros73@gmail.com>
2023-07-26 05:41:16 +0300

    prints the title of a Star Wars movie based on the provided movie ID

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,27 +1,16 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-function getMovieTitle (movieId) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-  fs.get(url)
-    .then((response) => {
-      const movieTitle = response.data.title;
-      console.log(movieTitle);
-    })
-    .catch((error) => {
-      if (error.response && error.response.status === 404) {
-        console.error(`Movie with ID ${movieId} not found.`);
-      } else {
-        console.error(`Error: ${error.message}`);
-      }
-    });
-}
-
-// Check if the script is run from the command line with the movie ID argument
-if (process.argv.length !== 3) {
-  console.error('Usage: ./3-starwars_title.js <movie_ID>');
-} else {
-  const movieId = process.argv[2];
-  getMovieTitle(movieId);
-}
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
