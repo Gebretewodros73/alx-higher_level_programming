ce07b93b8bad20b59a0257d59dd35eda40cc67a7
gebretewodros73 <gebretewodros73@gmail.com>
2023-07-26 05:35:58 +0300

    script displays the status code of a GET request to the provided URL

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,21 +1,5 @@
 #!/usr/bin/node
-const https = require('https');
-
-function displayStatusCode (url) {
-  https
-    .get(url, (response) => {
-      const statusCode = response.statusCode;
-      console.log(`code: ${statusCode}`);
-    })
-    .on('error', (error) => {
-      console.error(`Error: ${error.message}`);
-    });
-}
-
-// Check if the script is run from the command line with the URL argument
-if (process.argv.length !== 3) {
-  console.error('Usage: ./2-statuscode.js <URL>');
-} else {
-  const url = process.argv[2];
-  displayStatusCode(url);
-}
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
