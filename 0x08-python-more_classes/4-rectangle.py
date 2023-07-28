282e7da05b23dab690ce24f88297246bf5a88a49
gebretewodros73 <gebretewodros73@gmail.com>
2023-07-28 11:54:33 +0300

    class Rectangle that defines a rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Module: Class  Rectangle that defines a rectangle
+"""
+
+
 class Rectangle:
+    """
+    Class: class Rectangle that defines a rectangle
+    """
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
