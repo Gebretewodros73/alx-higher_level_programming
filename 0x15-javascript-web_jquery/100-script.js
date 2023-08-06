4de66a597367148aef8d90e3e4b7c150dc450d6d
gebretewodros73 <gebretewodros73@gmail.com>
2023-08-06 11:54:16 +0300

    update the text color of the <header> element to red (#FF0000) using document.querySelector without using the jQuery API.

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Use document.querySelector to select the <header> element
+const headerElement = document.querySelector('header');
+
+// Update the text color to red (#FF0000)
+headerElement.style.color = '#FF0000';
+
